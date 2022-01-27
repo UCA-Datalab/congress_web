ef898c9e4b47d3f83131fd76668080881f447a41
Javier-Jimenez99 <javijj99@gmail.com>
2022-01-27 11:42:45 +0100

    add form

---
+++ form_manager.js
@@ -0,0 +1,34 @@
+function ValidateEmail(input) {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (input.value.match(validRegex)) {
+
+        alert("Valid email address!");
+
+        document.form1.text1.focus();
+
+        return true;
+
+    } else {
+
+        alert("Invalid email address!");
+
+        document.form1.text1.focus();
+
+        return false;
+
+    }
+
+}
+
+const form = document.getElementById('registration');
+
+form.addEventListener('submit', (event) => {
+    email = form.elements["email"]
+
+    // stop form submission
+    //if (!ValidateEmail(email))
+    //event.preventDefault();
+
+    form.submit()
+});
